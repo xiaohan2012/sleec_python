94547c99c99fe1d2f35bc111a339db1d46f13dda
Han Xiao <han.xiao@aalto.fi>
2017-11-08 20:37:58 +0000

    kNN with weight

--- ensemble.py
+++ ensemble.py
@@ -21,11 +21,19 @@ class Model():
     def predict(self, x):
         model = self.closet_cluster(x)
         z = project(model['V'], x)
-        nbrs = model['Z_neighbors'].kneighbors([z], return_distance=False)[0]
-        real_idx = [model['data_idx'][i] for i in nbrs]
-        
-        label_scores = np.array(self.train_Y[real_idx, :].sum(axis=0)).flatten()
-        return label_scores
+        dist, nbrs = model['Z_neighbors'].kneighbors([z], return_distance=True)
+        real_idx = [model['data_idx'][i] for i in nbrs[0]]
+
+        # weight by 1 / distance
+        weights = (1 / dist).T
+        labels = np.asarray(self.train_Y[real_idx, :].todense())
+        # print(weights.shape)
+        # print(labels.shape)
+        # print(type(weights))
+        # print(type(labels))
+        scores_per_instance = labels * weights
+        scores = scores_per_instance.sum(axis=0)
+        return np.array(scores).flatten()
 
 
 class Ensemble():
